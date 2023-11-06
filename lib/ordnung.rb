02683e4d9e12d58e663c433a679e75868180de80
Klaus Kämpf <kkaempf@gmail.com>
2023-11-06 08:49:39 +0100

    Restart from scratch
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- lib/ordnung.rb
+++ lib/ordnung.rb
@@ -4,9 +4,7 @@ unless defined?(ORDNUNG_TOPLEVEL)
   ORDNUNG_TOPLEVEL = File.expand_path("..", File.dirname(__FILE__))
 end
 
-require_relative "ordnung/directory"
-require_relative "ordnung/mime_type"
-require_relative "ordnung/file"
+require_relative "ordnung/gizmo"
 require_relative "ordnung/logger"
 require_relative "ordnung/tag"
 require_relative "ordnung/version"
@@ -15,34 +13,8 @@ module Ordnung
 
   extend self
 
-  def database= database
-    Directory.database = database
-    MimeType.database = database
-    File.database = database
-    Tag.database = database
-    Edge.database = database
-  end
-  def setup
-    Directory.setup
-    MimeType.setup
-    File.setup
-    Tag.setup
-    Edge.setup
-  end
-  def cleanup
-    Edge.cleanup
-    Tag.cleanup
-    File.cleanup
-    MimeType.cleanup
-    Directory.cleanup
-  end
-
   def logger
     @@logger ||= Logger.new
-  end
-
-  def mimetype
-    @@mimetype ||= MimeType.new
   end
   
   class Ordnung
