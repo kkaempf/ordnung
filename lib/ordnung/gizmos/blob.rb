a718487290435ce59b424de67857189a0cc8250e
Klaus Kämpf <kkaempf@gmail.com>
2023-11-10 12:19:44 +0100

    Allow Blob logging
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- lib/ordnung/gizmos/blob.rb
+++ lib/ordnung/gizmos/blob.rb
@@ -6,8 +6,9 @@ module Ordnung
     def self.properties
       nil
     end
-    def initialize path, parent
-      super path, parent
+    def initialize path, parent_id
+#      Gizmo.log.info "Blob.new(#{path.inspect}, #{parent_id.inspect})"
+      super path, parent_id
     end
   end
 end
